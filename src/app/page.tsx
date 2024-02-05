8b812718e399834601ba738616420b294c92563d
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-02-05 16:36:30 +0900

    added grocery list skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
+const categories = [
+  "Food",
+  "Toiletries",
+  "HealthCare",
+  "Household and Cleaning Supplies"
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Jones Person 
+    <main className="flex text-base font-semibold min-h-screen flex-col items-center justify-between p-24 text-slate-700">
+     <h1 className="text-3xl"> 
+      Extremely Simple Grocery Shopping List
+     </h1>
+     <input placeholder="item to buy" className="text-base shadow-lg border p-4" />
+     <select>
+      <option>Food</option>
+      <option>Toiletries</option>
+      <option>Healthcare</option>
+      <option>Household and Cleaning Supplies</option>
+     </select>
+     <button className="bg-amber-300 p-4 rounded-lg text-white font-bold uppercase hover:opacity-75">
+      Save
+     </button>
+
+     <ul>
+      <h1 className="text-xl">Search by Category:</h1>
+      {categories.map((category, i) => <li key={category+i} className="text-xl text-sky-700">{category}</li>)}
+     </ul>
+     <div className="text-lg text-center pt-2 p-4">Because the groceries won't buy themselves</div>
     </main>
   );
 }
